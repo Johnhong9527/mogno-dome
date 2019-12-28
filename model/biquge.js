aa93c48a9f17754fe7dd24b7f9e774cbf68d3986
feishu <honghaitzz11@gmail.com>
2019-12-28 18:37:23 +0800

    更新数据模型

--- model/biquge.js
+++ model/biquge.js
@@ -7,45 +7,26 @@
 const mongoose = require('mongoose');
 const mongo = require('../util/mongo.js');
 const Schema = mongoose.Schema;
-/**
- * [BooksSchema description]
- * 书名 --> title str
- * 来源 --> source str (各大小说网站)
- * 作者 --> author str
- * 书籍ID --> aid num
- * 书籍唯一ID --> openID = _id
- * 最后更新时间 --> latest update str
- * 最新章节 --> latest chapter num
- * 状态 --> status 0:连载 1:完结
- * 介绍 --> intro str
- * 排序 --> index num
- * 章节列表 --> chapters Arr
- *   章节 -->
- *     章节id
- *     章节名title
- * @type {Schema}
- */
+
 const BooksSchema = new Schema({
-	title: { type: String },
-	source: { type: String },
-	author: { type: String },
-	avatar: { type: String },
-	tag: { type: String },
-	tag_url: { type: String },
-	aid: { type: Number },
-	lastUpdate: { type: Number },
-	status: { type: Number },
-	intro: { type: String },
-	index: { type: Number },
-	href: { type: String },
-	open_id: { type: String },
+	title: { type: String }, // 书名（不同书籍不可重复）
+	source: { type: String }, // 来源(各大小说网站)
+	author: { type: String }, //作者
+	avatar: { type: String }, // 封面
+	tag: { type: String }, // 标签
+	tag_url: { type: String }, // 标签链接
+	aid: { type: Number }, // 书籍原bookID（不同书籍不可重复）
+	lastUpdate: { type: Number }, // 书籍最后更新时间
+	status: { type: Number }, // 书籍更新进度
+	intro: { type: String }, // 书籍简介
+	index: { type: Number }, // 书籍排序
+	href: { type: String }, // 书籍原链接
+	open_id: { type: String }, // 书籍唯一ID（不同书籍不可重复）
 	chapters: {
 		type: Array,
-		title: { type: String },
-		path: { type: String },
-		aid: { type: Number },
-		cid: { type: Number },
-		href: { type: String },
+		title: { type: String }, // 章节名（不同书籍可重复）
+		cid: { type: Number }, // 章节ID（不同书籍可重复）
+		href: { type: String }, // 章节原地址
 	},
 });
 const Books = mongo.biquge.model('Books', BooksSchema);
--- model/chapters.js
+++ model/chapters.js
@@ -5,22 +5,17 @@
  */
 
 const mongoose = require('mongoose');
-const mongo = require('../util/mongo.js')
+const mongo = require('../util/mongo.js');
 const Schema = mongoose.Schema;
 
 const ChapterSchema = new Schema({
-  title: { type: String },
-  path: { type: String },
-  aid: { type: Number },
-  cid: { type: Number },
-  href: { type: String },
-  index: { type: Number },
-  content: { type: String },
+	title: { type: String },
+	path: { type: String },
+	book_id: { type: String },
+	cid: { type: Number },
+	href: { type: String },
+	content: { type: String },
 });
-
-/*ChapterSchema.add({
-  index: { type: Number },
-});*/
 
 const Chapter = mongo.xxxbz.model('Chapter', ChapterSchema);
 module.exports = Chapter;
